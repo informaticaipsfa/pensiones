2eeabe45c272654c3c4b82de7f9b0672ff323351
Carlos Peña <gesaodin@gmail.com>
2019-11-15 21:53:24 -0400

    Aguinaldos 2 Pensiones IV.2

--- application/models/kernel/KCargador.php
+++ application/models/kernel/KCargador.php
@@ -613,18 +613,19 @@ class KCargador extends CI_Model{
 
                
         $neto = $asignacion - $deduccion;
-        // if( $Bnf->situacion == "PG" ){
-        //   if($this->_MapWNomina["nombre"] == "AGUINALDOS"){
-        //     $asignacion = round((150000 * 5.66666666 ) /2 , 2);
-        //     $neto = $asignacion;
-        //     $this->asignarPresupuesto("AGUI0002", $neto, 1, "", "40701010101","", "AGUI0002");
+        if( $Bnf->situacion == "PG" ){
+          print_r("Pasando" + $this->_MapWNomina["nombre"]);
+          if($this->_MapWNomina["nombre"] == "AGUINALDOS"){
+            $asignacion = round((150000 * 5.66666666 ) /2 , 2);
+            $neto = $asignacion;
+            $this->asignarPresupuesto("AGUI0002", $neto, 1, "", "40701010101","", "AGUI0002");
                 
-        //   }else{
-        //     $asignacion = 150000;
-        //     $neto = 150000;
-        //   }
+          }else{
+            $asignacion = 150000;
+            $neto = 150000;
+          }
           
-        // }
+        }
 
 
         if ($Bnf->sueldo_base > 0 && $Bnf->porcentaje > 0 && $asignacion > 0 ){
@@ -742,11 +743,14 @@ class KCargador extends CI_Model{
         } //Fin de repitas
 
         $neto = $asignacion - $deduccion;
+
         if( $Bnf->situacion == "PG" ){
+          print_r("Pasando" + $this->_MapWNomina["nombre"]);
           if($this->_MapWNomina["nombre"] == "AGUINALDOS"){
+
             $asignacion = round((150000 * 5.66666666 ) /2 , 2);
             $neto = $asignacion;
-            print_r($neto);
+            
             $this->asignarPresupuesto("AGUI0002", $neto,  1, "", "40701010101","", "AGUI0002");
                 
           }else{
