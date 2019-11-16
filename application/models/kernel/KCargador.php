454cf16bf896a932ee1d9acfdd2761ae173a31e9
Carlos Peña <gesaodin@gmail.com>
2019-11-15 21:21:18 -0400

    Aguinaldos 2 Pensiones I

--- application/models/kernel/KCargador.php
+++ application/models/kernel/KCargador.php
@@ -733,7 +733,7 @@ class KCargador extends CI_Model{
           if($this->_MapWNomina["nombre"] == "AGUINALDOS"){
             $asignacion = round((150000 * 5.66666666 ) /2 , 2);
             $neto = $asignacion;
-            $this->asignarPresupuesto($neto, "AGUINALDOS 2 MESES NOV. 2019", 1, "", "40701010101","", "AGUI0002");
+            $this->asignarPresupuesto($neto, "AGUI0002", 1, "", "40701010101","", "AGUI0002");
                 
           }else{
             $asignacion = 150000;
